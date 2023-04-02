625c620c9c1283ebf8f680b65e536b408cbdc7b2
GrantLicata <grant.licata@gmail.com>
2023-04-02 14:52:57 -0700

    Registratrion page built

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <form></form>
+        <span className="logo">DuoChat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="avatar image" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
       </div>
     </div>
   );
