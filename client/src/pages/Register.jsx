7b079b2c7913f59c7d1b1c07777567d2127a1ccf
GrantLicata <grant.licata@gmail.com>
2023-04-01 22:57:06 -0700

    Register page progress

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <form></form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
