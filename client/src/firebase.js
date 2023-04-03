dc886b233fa54a1f2b52824a6f37097e5ec83a37
GrantLicata <grant.licata@gmail.com>
2023-04-02 18:22:39 -0700

    Firebase initialized

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYYA_vyfJ0iHqbEZwzqROz44g7l_gqep4",
+  authDomain: "duochat-10001.firebaseapp.com",
+  projectId: "duochat-10001",
+  storageBucket: "duochat-10001.appspot.com",
+  messagingSenderId: "199364626952",
+  appId: "1:199364626952:web:4a0aa46b5e98bceadddfbd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
