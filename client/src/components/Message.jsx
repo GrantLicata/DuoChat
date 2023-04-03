827f2a1bb5fcba1b8c857bd724ac8036d91b9b90
GrantLicata <grant.licata@gmail.com>
2023-04-02 18:17:24 -0700

    UI initial development complete

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Message image"
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        {/* <img
+          src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Chat image"
+        /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
