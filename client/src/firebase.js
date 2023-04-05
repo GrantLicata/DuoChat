4f78f38d5f2828b7d5bbadd07ba26a937b7a019b
GrantLicata <grant.licata@gmail.com>
2023-04-05 10:50:23 -0700

    Plugged in firestore database

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDYYA_vyfJ0iHqbEZwzqROz44g7l_gqep4",
@@ -10,6 +12,10 @@ const firebaseConfig = {
   appId: "1:199364626952:web:4a0aa46b5e98bceadddfbd",
 };
 
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
 import Add from "../img/addAvatar.png";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth, storag, db } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
+  // Managing error states
   const [err, setErr] = useState(false);
 
+  // Submission of user information for registration
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    // const file = e.target[3].file[0];
+    const file = e.target[3].file[0];
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+
+      await setDoc(doc(db, "users", res.user.uid));
     } catch (err) {
       setErr(true);
     }
