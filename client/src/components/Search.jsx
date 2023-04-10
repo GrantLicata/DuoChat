7567ffd0bc261b40b6dd99c40fd9794cbdcc609c
GrantLicata <grant.licata@gmail.com>
2023-04-09 17:22:07 -0700

    User search built

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    // Query database for users that match the username state.
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      // Get database document that matches the query and set that as the user identified within the search.
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      // Capture any errors produced from database call
+      setErr(true);
+      console.log(err);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="User profile image"
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <div className="userChatInfo">
-          <span>Jane</span>
-        </div>
       </div>
+      {/* If no user found, then error is displayed */}
+      {err && <span>User not found!</span>}
+      {/* User displayed if found within search */}
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="User profile image" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
